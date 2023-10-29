87234b1e0f4bdebfd4d6583704c8383482ee36da
jdww5567 <jadaniel06@gmail.com>
2023-10-28 23:23:38 -0500

    back to starting point

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(parseInt(localStorage.getItem('savedCounter')));
+
+  if (counter != counter) {
+    setCounter(0);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('savedCounter', counter);
+  }, [counter]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Counter App</h1>
+      <div className='centeredContainer'>
+        <button className='buttonItems' onClick={() => setCounter(counter - 1)}>-</button>
+        <h2 className='buttonItems'>{counter}</h2>
+        <button className='buttonItems' onClick={() => setCounter(counter + 1)}>+</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
