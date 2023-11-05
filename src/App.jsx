635c9c89506844a8d59d90838115e674c44d6158
jdww5567 <jadaniel06@gmail.com>
2023-11-04 21:21:34 -0500

    switch to plotly for 3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from 'react'
-import { Line } from 'react-chartjs-2';
-import { Chart } from 'chart.js/auto';
+import React, { useState, useEffect } from 'react';
+import Plot from 'react-plotly.js';
 
-import './App.css'
+import './App.css';
 
 const generateChartData = (A, B, chartLength) => {
   let arithmeticGData = [A];
@@ -12,86 +11,117 @@ const generateChartData = (A, B, chartLength) => {
   let labels = [];
 
   for (let i = 1; i <= chartLength; i++) {
-    arithmeticGData[i] = (arithmeticGData[i-1] + geometricAData[i-1]) / 2;
-    geometricAData[i] = Math.sqrt(arithmeticGData[i-1] * geometricAData[i-1]);
-    harmonicGData[i] = 2 * (harmonicGData[i-1] * geometricHData[i-1]) / (harmonicGData[i-1] + geometricHData[i-1]);
-    geometricHData[i] = Math.sqrt(harmonicGData[i-1] * geometricHData[i-1]);
+    arithmeticGData[i] = (arithmeticGData[i - 1] + geometricAData[i - 1]) / 2;
+    geometricAData[i] = Math.sqrt(arithmeticGData[i - 1] * geometricAData[i - 1]);
+    harmonicGData[i] = (2 * harmonicGData[i - 1] * geometricHData[i - 1]) / (harmonicGData[i - 1] + geometricHData[i - 1]);
+    geometricHData[i] = Math.sqrt(harmonicGData[i - 1] * geometricHData[i - 1]);
+    labels.push(i - 1);
   }
+  labels.push(chartLength);
 
-  let arithmeticMean = [arithmeticGData[1]];
-  let geometricMean = [geometricAData[1]];
-  let harmonicMean = [harmonicGData[1]];
+  let zeroes = Array(chartLength + 1).fill(0);
 
-  for (let i = 0; i <= chartLength; i++) {
-    labels[i] = i;
-    arithmeticMean[i + 1] = arithmeticMean[i];
-    geometricMean[i + 1] = geometricMean[i];
-    harmonicMean[i + 1] = harmonicMean[i];
-  }
+  let arithmeticMean = Array(2).fill(arithmeticGData[1]);
+  let geometricMean = Array(2).fill(geometricAData[1]);
+  let harmonicMean = Array(2).fill(harmonicGData[1]);
+
+  let chartBoundsX = [0, chartLength];
+  let chartBoundsY = [0, 0];
 
   return {
-    labels: labels,
-    datasets: [
+    x: labels,
+    data: [
       {
-        label: 'Arithmetic-G Sequence',
-        data: arithmeticGData,
-        backgroundColor: 'rgba(70, 100, 160, 0.2)',
-        borderColor: 'rgba(70, 100, 160, 1)',
-        borderWidth: 1
+        type: 'scatter3d',
+        mode: 'lines+markers',
+        name: 'Arithmetic-G Sequence',
+        x: labels,
+        y: zeroes,
+        z: arithmeticGData,
+        line: { color: 'rgba(70, 100, 160, 1)' },
       },
       {
-        label: 'Geometric-A Sequence',
-        data: geometricAData,
-        backgroundColor: 'rgba(200, 75, 100, 0.2)',
-        borderColor: 'rgba(200, 75, 100, 1)',
-        borderWidth: 1
+        type: 'scatter3d',
+        mode: 'lines+markers',
+        name: 'Geometric-A Sequence',
+        x: labels,
+        y: zeroes,
+        z: geometricAData,
+        line: { color: 'rgba(200, 75, 100, 1)' }
       },
       {
-        label: 'Harmonic-G Sequence',
-        data: harmonicGData,
-        backgroundColor: 'rgba(130, 140, 86, 0.2)',
-        borderColor: 'rgba(130, 140, 86, 1)',
-        borderWidth: 1
+        type: 'scatter3d',
+        mode: 'lines+markers',
+        name: 'Harmonic-G Sequence',
+        x: labels,
+        y: zeroes,
+        z: harmonicGData,
+        line: { color: 'rgba(130, 140, 86, 1)' }
       },
       {
-        label: 'Geometric-H Sequence',
-        data: geometricHData,
-        backgroundColor: 'rgba(200, 80, 88, 0.2)',
-        borderColor: 'rgba(200, 80, 88, 1)',
-        borderWidth: 1
+        type: 'scatter3d',
+        mode: 'lines+markers',
+        name: 'Geometric-H Sequence',
+        x: labels,
+        y: zeroes,
+        z: geometricHData,
+        line: { color: 'rgba(200, 80, 88, 1)' }
       },
       {
-        label: 'Arithmetic Mean',
-        data: arithmeticMean,
-        backgroundColor: 'rgba(45, 112, 176, 0.2)',
-        borderColor: 'rgba(45, 112, 176, 1)',
-        borderWidth: 2,
-        pointRadius: 0
+        type: 'scatter3d',
+        mode: 'lines',
+        name: 'Arithmetic Mean',
+        x: chartBoundsX, 
+        y: chartBoundsY, 
+        z: arithmeticMean, 
+        line: { color: 'blue' }
+
       },
       {
-        label: 'Geometric Mean',
-        data: geometricMean,
-        backgroundColor: 'rgba(228, 63, 90, 0.2)',
-        borderColor: 'rgba(228, 63, 90, 1)',
-        borderWidth: 2,
-        pointRadius: 0
+        type: 'scatter3d',
+        mode: 'lines',
+        name: 'Geometric Mean',
+        x: chartBoundsX,
+        y: chartBoundsY,
+        z: geometricMean,
+        line: { color: 'red' }
       },
       {
-        label: 'Harmonic Mean',
-        data: harmonicMean,
-        backgroundColor: 'rgba(117, 168, 84, 0.2)',
-        borderColor: 'rgba(117, 168, 84, 1)',
-        borderWidth: 2,
-        pointRadius: 0
-      }
-    ]
+        type: 'scatter3d',
+        mode: 'lines',
+        name: 'Harmonic Mean',
+        x: chartBoundsX, 
+        y: chartBoundsY, 
+        z: harmonicMean, 
+        line: { color: 'green' }
+      },
+    ],
   };
 };
 
+const chartLayout = {
+    scene: {
+      aspectmode: 'manual',
+      aspectratio: { x: 1, y: 1, z: 0.5 },
+      xaxis: {
+        title: 'Iteration'
+      },
+      yaxis: {
+        title: 'Real'
+      },
+      zaxis: {
+        title: 'Complex'
+      }
+    },
+    legend: {
+      orientation: 'h'
+    }
+};
+
 function App() {
-  const [A, setA] = useState(0);
-  const [B, setB] = useState(0);
-  const [chartData, setChartData] = useState(generateChartData(A, B));
+  const [A, setA] = useState(1);
+  const [B, setB] = useState(1);
+  const [chartData, setChartData] = useState(generateChartData(A, B, 4));
   const [chartLength, setChartLength] = useState(4);
 
   useEffect(() => {
@@ -104,26 +134,24 @@ function App() {
         <input
           type="number"
           defaultValue={A}
-          onChange = {(event) => setA(Math.abs(Number(event.target.value)))}
+          onChange={(event) => setA(Number(event.target.value))}
         />
         <input
           type="number"
           defaultValue={B}
-          onChange = {(event) => setB(Math.abs(Number(event.target.value)))}
+          onChange={(event) => setB(Number(event.target.value))}
         />
         <input
           type="number"
           defaultValue={chartLength}
-          onChange = {(event) => setChartLength(Math.abs(Number(event.target.value)))}
+          onChange={(event) => setChartLength(Math.abs(Number(event.target.value)))}
         />
       </div>
       <div className='centeredContainer'>
-        <div className='chartContainer'>
-          <Line data={chartData}/>
-        </div>
+        <Plot id='stfu' data={chartData.data} layout={chartLayout} />
       </div>
     </>
   );
 }
 
-export default App
+export default App;
